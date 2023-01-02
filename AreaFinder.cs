75a2160ef88c8557a7ba8a27b918770a924d9e77
Bulat Musin <91786508+doge1488@users.noreply.github.com>
2023-01-02 17:35:45 +0300

    Update AreaFinder.cs

--- AreaFinder.cs
+++ AreaFinder.cs
@@ -1,3 +1,4 @@
+
 using System;
 
 namespace AreaFinder
@@ -5,11 +6,20 @@ namespace AreaFinder
     internal class Program
     {
         static void Main(string[] args)
-        {   
+        {
             Circle circle = new Circle("Circle", 2);
-            Triangle triangle = new Triangle("Triangle", 3, 4, 5);
-            Console.WriteLine(circle.CalcSquare());
-            Console.WriteLine(triangle.CalcSquare());
+            Triangle triangle = new Triangle("Triangle", 3, 5, 5);
+            Console.WriteLine($"Your circle area is: " + circle.CalcSquare());
+            Console.WriteLine($"Your triangle area is: "+triangle.CalcSquare());
+            if (Math.Pow(triangle.x, 2) == Math.Pow(triangle.y, 2) + Math.Pow(triangle.z, 2) || Math.Pow(triangle.y, 2) == Math.Pow(triangle.x, 2) + Math.Pow(triangle.z, 2) || Math.Pow(triangle.z, 2) == Math.Pow(triangle.y, 2) + Math.Pow(triangle.x, 2))
+            {
+                Console.WriteLine("Your triangle is right-angled");
+            }
+            else
+            {
+                Console.WriteLine("Your triangle is not right-angled");
+            }
+
         }
 
         
